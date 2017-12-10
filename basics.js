63e4192e1d686d41ac6a16eb5dc3449ffa177664
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2017-12-09 15:54:11 -1000

    myName test pass

--- basics.js
+++ basics.js
@@ -1,5 +1,7 @@
 /* Create a `myName` variable and assign it a String value */
 
+var myName = "Jocelyn";
+
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
  * as well as an `age` (number);
@@ -9,10 +11,12 @@
  * if it should be true if your person object is at least 16 years old
  */
 
+
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
+
 
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
